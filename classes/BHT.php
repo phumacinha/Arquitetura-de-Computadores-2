ff6e334f54e2674a1e997173bdbf9f258d238473
Pedro Antônio <pedro.souza2@estudante.ufla.br>
2019-11-19 20:55:27 -0300

    adding counters

--- classes/BHT.php
+++ classes/BHT.php
@@ -7,20 +7,61 @@ class BHT {
     private $m;
     private $bht;
     private $iter;
+    private $counter;
     private $file;
 
 
     public function __construct ($m, $n, $initialValue, $file) {
         $this->n = $n;
-        $this->m = $m;
+        $this->m = log($m, 2);
         $this->bht = [];
         $this->iter = [];
+        $this->counter = [];
         $this->file = $file;
 
         $history = $n == 2 ? [$initialValue, $initialValue] : [$initialValue];
 
         for ($i = 0; $i < $m; $i++) {
             array_push($this->bht, ["history"=>$history, "prediction"=>$initialValue, "correct"=>0, "incorrect"=>0, "precision"=>0]);
+            
+            if ($this->n == 1 && $initialValue == TAKE) {
+                array_push($this->counter, 1);
+            }
+
+            if ($this->n == 2 && $initialValue == TAKE) {
+                array_push($this->counter, 3);
+            }
+
+            if ($initialValue == NOT_TAKE) {
+                array_push($this->counter, 0);
+            }
+            
+        }
+    }
+
+    private function updateCounter ($index, $branch) {
+        switch ($branch) {
+            case TAKE:
+                switch ($this->n) {
+                    case 1:
+                        //1-bit predictor
+                        //counter saturates at 1
+                        $this->counter[$index] = 1;
+                    break;
+
+                    case 2:
+                        //2-bit predictor
+                        //counter saturates at 3
+                        if (++$this->counter[$index] > 3)
+                            $this->counter[$index] = 3;
+                    break;
+                }
+            break;
+
+            case NOT_TAKE:
+                if (--$this->counter[$index] < 0)
+                    $this->counter[$index] = 0;
+            break;
         }
     }
 
@@ -28,8 +69,7 @@ class BHT {
         flush();
         $data = fopen($this->file, "r");
         
-        while(!feof($data))
-        {
+        while (!feof($data)) {
             $trace = fgets($data);
             $trace = explode(" ", $trace);
 
@@ -37,7 +77,7 @@ class BHT {
             $branch = trim($trace[1]);
 
             $shift = substr(base_convert($address, 16, 2), 0, -2);
-            $index = substr($shift, -log($this->m, 2));
+            $index = substr($shift, -$this->m);
             $decimalIndex = base_convert($index, 2, 10);
 
             $line = &$this->bht[$decimalIndex];
@@ -70,13 +110,10 @@ class BHT {
                     break;
             }
             
-            
             $line["precision"] =  $line["correct"]/($line["correct"] + $line["incorrect"]);
 
             array_push($historic, $line);
             array_push($this->iter, [$correct, $decimalIndex, $historic, $address]);
-
-            
 
             flush();
         }
--- index.php
+++ index.php
@@ -12,38 +12,38 @@
 <body>
     <header>BHT SIMULATOR</header>
     <menu>
-    <form id="data" method="post">
-        <div class="file">Select a file: <input type="file" name="file" id="file" required></div>
-        
-        <div class="lineSelects">
-            <div># of BHT entries:
-                <select name="m" id="m">
-                    <option value="2">2</option>
-                    <option value="4">4</option>
-                    <option value="8">8</option>
-                    <option value="16">16</option>
-                    <option value="32">32</option>
-                    <option value="64">64</option>
-                </select>
+        <form id="data" method="post">
+            <div class="file">Select a file: <input type="file" name="file" id="file" required></div>
+            
+            <div class="lineSelects">
+                <div># of indexes:
+                    <select name="m" id="m">
+                        <option value="2">2</option>
+                        <option value="4">4</option>
+                        <option value="8">8</option>
+                        <option value="16">16</option>
+                        <option value="32">32</option>
+                        <option value="64">64</option>
+                    </select>
+                </div>
+                <div>History size:
+                    <select name="n" id="n">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                    </select>
+                </div>
+                <div>Initial value:
+                    <select name="initialValue" id="initialValue">
+                        <option value="T">TAKE</option>
+                        <option value="N">NOT TAKE</option>
+                    </select>
+                </div>
             </div>
-            <div>BHT history size:
-                <select name="n" id="n">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                </select>
-            </div>
-            <div>Initial value:
-                <select name="initialValue" id="initialValue">
-                    <option value="T">TAKE</option>
-                    <option value="N">NOT TAKE</option>
-                </select>
-            </div>
-        </div>
-        
+            
 
-        <input type="submit" value="Simulate">
-    </form>
-</menu>
+            <input type="submit" value="Simulate">
+        </form>
+    </menu>
 
     <script src="js/bht.js"></script>
 
